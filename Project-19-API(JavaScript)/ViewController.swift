82556434fd43e1e3f27d48d64961f8bc3c88fbed
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-16 16:05:55 +0200

    Initial Commit

---
+++ Project-19-API(JavaScript)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-19-API(JavaScript)
+//
+//  Created by Serhii Prysiazhnyi on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
