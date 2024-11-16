d73b7d2f2bcd8a97de3ccb61182583e5ff8f23b1
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-11-16 22:09:27 +0200

    add-save

--- Extension/ActionViewController.swift
+++ Extension/ActionViewController.swift
@@ -15,6 +15,7 @@ class ActionViewController: UIViewController {
     
     var pageTitle = ""
     var pageURL = ""
+    //var nameScript = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,28 +38,59 @@ class ActionViewController: UIViewController {
             }
         }
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Скрипты", style: .plain, target: self, action: #selector(showScriptList))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(transitionAction))
         
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-
-        DispatchQueue.main.async {
-            self.selectAlert()
-        }
-
         
+        loadScriptsList()
+        
+        DispatchQueue.main.async {
+           self.selectAlert()  //-- алерт с дефолтными скриптами
+        }
     }
 
+    @objc func transitionAction() {
+        if !script.text.isEmpty {
+            let savedScripts = loadScriptsList() // Получаем список сохранённых скриптов
+            
+            // Проверяем, есть ли скрипт с таким же содержимым в списке сохранённых
+            if let scripts = savedScripts {
+                if scripts.values.contains(script.text) {
+                    // Если скрипт уже сохранён, просто завершаем
+                    done()
+                    print("Скрипт уже сохранён")
+                } else {
+                    // Если скрипт не найден в сохранённых, вызываем alert для сохранения
+                    alertBeforSaveScript(script: script.text)
+                }
+            } else {
+                // Если список сохранённых скриптов пуст, тоже вызываем alert для сохранения
+                alertBeforSaveScript(script: script.text)
+            }
+        } else {
+            // Завершаем, если текст пустой
+            done()
+            print("Пусто")
+        }
+    }
+
+    
     @IBAction func done() {
-        let item = NSExtensionItem()
-        let argument: NSDictionary = ["customJavaScript": script.text]
-        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
-        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
-        item.attachments = [customJavaScript]
+        
+            guard let host = URL(string: pageURL)?.host else { return }
 
-        extensionContext?.completeRequest(returningItems: [item])
-    }
+            // Передача данных хост-приложению
+            let item = NSExtensionItem()
+            let argument: NSDictionary = ["customJavaScript": script.text]
+            let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+            let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+            item.attachments = [customJavaScript]
+            
+            extensionContext?.completeRequest(returningItems: [item])
+        }
 
     @objc func adjustForKeyboard(notification: Notification) {
         guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
@@ -77,29 +109,6 @@ class ActionViewController: UIViewController {
         let selectedRange = script.selectedRange
         script.scrollRangeToVisible(selectedRange)
     }
-
-//    func selectAlert() {
-//        
-//        let alert = UIAlertController(title: "Choose default script", message: "Select from the list", preferredStyle: .actionSheet)
-//        
-//        // Добавляем действия для разных типов карт
-//        alert.addAction(UIAlertAction(title: "alert(document.title);", style: .default, handler: { _ in
-//            self.done()
-//        }))
-//        
-////        alert.addAction(UIAlertAction(title: "Satellite", style: .default, handler: { _ in
-////            self.mapView.mapType = .satellite
-////        }))
-////        
-////        alert.addAction(UIAlertAction(title: "Hybrid", style: .default, handler: { _ in
-////            self.mapView.mapType = .hybrid
-////        }))
-//        
-//        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-//        
-//        // Показываем UIAlertController
-//        present(alert, animated: true, completion: nil)
-//    }
     
     func selectAlert() {
         DispatchQueue.main.async {
@@ -117,21 +126,79 @@ class ActionViewController: UIViewController {
                 self.script.text = "alert(document.domain);"
                 self.done()
             }))
-        alert.addAction(UIAlertAction(title: "alert(document.body.innerHTML);", style: .default, handler: { _ in
-            self.script.text = "alert(document.body.innerHTML);"
-            self.done()
-        }))
+            alert.addAction(UIAlertAction(title: "alert(document.body.innerHTML);", style: .default, handler: { _ in
+                self.script.text = "alert(document.body.innerHTML);"
+                self.done()
+            }))
             
             alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
             
-                  //  для IPAD
-//            if let popoverController = alert.popoverPresentationController {
-//                popoverController.sourceView = self.view
-//                popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
-//                popoverController.permittedArrowDirections = []
-//            }
+            //  для IPAD
+            //            if let popoverController = alert.popoverPresentationController {
+            //                popoverController.sourceView = self.view
+            //                popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+            //                popoverController.permittedArrowDirections = []
+            //            }
             
             self.present(alert, animated: true, completion: nil)
+        }
+    }
+        
+    func alertBeforSaveScript(script: String) {
+        let ac = UIAlertController(title: "Задать название скрипта", message: nil, preferredStyle: .alert)
+        
+        let submitAction = UIAlertAction(title: "Сохранить", style: .default) { [weak self, weak ac] _ in
+            guard let nameScript = ac?.textFields?[0].text, !nameScript.isEmpty else { return }
+            self?.saveScript(name: nameScript, script: script)
+            print("name -- \(nameScript), script -- \(script)")
+            
+            // Завершить работу расширения только после сохранения
+            self?.done()
+        }
+        
+        ac.addTextField { textField in
+            textField.placeholder = "Введите название скрипта"
+        }
+        
+        ac.addAction(submitAction)
+        ac.addAction(UIAlertAction(title: "Отмена", style: .cancel)) // Не завершаем расширение на отмене
+        
+        present(ac, animated: true)
+    }
+
+
+    
+    func saveScript(name: String, script: String) {
+        let defaults = UserDefaults.standard
+        var scripts = defaults.dictionary(forKey: "SavedScripts") as? [String: String] ?? [:]
+        scripts[name] = script
+        defaults.set(scripts, forKey: "SavedScripts")
+        print("Скрипт \(name) сохранён.")
+    }
+    
+    func loadScriptsList() -> [String: String]? {
+        let defaults = UserDefaults.standard
+        if let savedScripts = defaults.dictionary(forKey: "SavedScripts") as? [String: String], !savedScripts.isEmpty {
+            print("Загружены скрипты: \(savedScripts)")  // Печать для отладки
+            return savedScripts
+        } else {
+            print("Скрипты не найдены.")
+            return nil
+        }
+    }
+
+    @objc func showScriptList() {
+        if let scripts = loadScriptsList() {
+            let alert = UIAlertController(title: "Выберите скрипт", message: nil, preferredStyle: .actionSheet)
+
+            for (name, _) in scripts {
+                alert.addAction(UIAlertAction(title: name, style: .default, handler: { _ in
+                    self.script.text = scripts[name]
+                }))
+            }
+            
+            alert.addAction(UIAlertAction(title: "Отмена", style: .cancel))
+            self.present(alert, animated: true)
         }
     }
 }
